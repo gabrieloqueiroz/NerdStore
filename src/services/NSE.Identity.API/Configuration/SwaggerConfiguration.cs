a73d097156edb4c0ccb7a539d8bfe8d1800c6356
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-03-17 02:01:27 -0300

    Adicionando documentacao com Swagger

---
+++ src/services/NSE.Identity.API/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NSE.Identity.API.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Nerd Store Enterprise Identity API",
+                    Description = "The purpose of this API is to register new accounts and authenticate created accounts",
+                    Contact = new OpenApiContact() { Name = "Gabriel Queiroz", Email = "gabriel@developer,com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                }));
+          
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app, string baseUrl, string serviceName)
+        {
+
+            app.UseSwagger(c => {
+                c.RouteTemplate = baseUrl + "/swagger/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/{baseUrl}/swagger/v1/swagger.json", serviceName);
+                c.RoutePrefix = $"{baseUrl}/swagger";
+            });
+
+            return app;
+        }
+
+    }
+}
--- src/services/NSE.Identity.API/Program.cs
+++ src/services/NSE.Identity.API/Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NSE.Identity.API.Data;
+using NSE.Identity.API.Configuration;
 using NSE.Identity.API.Model;
+using System.Runtime.CompilerServices;
+
+string baseUrl = "nse/identity";
+string serviceName = "Identity";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,16 +15,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddSwaggerConfiguration();
+
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
-
-
+app.UseSwaggerConfiguration(baseUrl, serviceName);
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
